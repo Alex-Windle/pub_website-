5164f14650a083f488bfb06ef91cbc78843d5633
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-16 16:30:47 -0400

    created json data of daily special and dynamically displayed it in html

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Javascript Entry Point
 import $ from 'jquery';
 
+import specials from './specials';
+
 var $menu = $('.menu');
 
 var url = "https://json-data.herokuapp.com/restaurant/menu/3";
@@ -14,8 +16,22 @@ $.ajax(url).then(function(object){
 	})
 
 	$menu.append(beerTitles);
-	console.log(object.Beer);
-
 
 });
 
+console.log(specials.beers);
+
+var $todaysSpecialHtml = $('.todays-special');
+
+function displaySpecial (specials){
+	var day = new Date();
+	day = day.getDay();
+
+	$todaysSpecialHtml.append(`<div>Beer: ${specials.beers[day].item}</div>
+								<div>Price: $${specials.beers[day].price}</div>
+								<div>Style: ${specials.beers[day].style}</div>
+								<div>${specials.beers[day].description}</div>
+								`);
+}
+
+displaySpecial(specials);
