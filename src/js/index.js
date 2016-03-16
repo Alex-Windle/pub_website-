c4c945cca117f413148c92c1e5f9ce25427c04bd
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-16 15:43:38 -0400

    data displaying dynamically from menu json

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 // Javascript Entry Point
 import $ from 'jquery';
 
-var $app = $('.app');
+var $menu = $('.menu');
 
 var url = "https://json-data.herokuapp.com/restaurant/menu/3";
 
-$.ajax(url).then(function(item){
+$.ajax(url).then(function(object){
+
+	var beerTitles = object.Beer.map(function({item, price, description}){
+		return `<div class="itemTitle">${item}</div>
+				<div class="itemPrice">${price}</div>
+				<div class="itemPrice">${description}</div>`;
+	})
+
+	$menu.append(beerTitles);
+	console.log(object.Beer);
+
 
 });
 
