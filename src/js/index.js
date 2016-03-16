15eac17d0a0c03427ff0d5c3a5a8342ceb0b1f22
Alex Windle <alexwindle85@gmail.com>
2016-03-16 15:23:50 -0400

    js changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 // Javascript Entry Point
 import $ from 'jquery';
+
+var $app = $('.app');
+
+var url = "https://json-data.herokuapp.com/restaurant/menu/3";
+
+$.ajax(url).then(function(item){
+
+});
+
