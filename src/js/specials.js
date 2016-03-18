f59312f2efc061fcb7d120dedfdeaf639947b3f6
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-18 13:03:13 -0400

    updated specials data to include product images for all specials

--- src/js/specials.js
+++ src/js/specials.js
@@ -6,7 +6,7 @@ export default {
 		price: 6,
 		style: "Russian Stout",
 		abv: 9,
-		imgUrl: "placeholder",
+		imgUrl: "https://s-media-cache-ak0.pinimg.com/236x/e3/bb/05/e3bb05d97eafa417aed558b0c2fbf89b.jpg",
 		description: "Rich, intense brew with big complex flavors and a warming finish."
 		},
 		{
@@ -15,7 +15,7 @@ export default {
 		price: 8,
 		style: "American Double / Imperial Stout",
 		abv: 11.2,
-		imgUrl: "placeholder",
+		imgUrl: "https://kingbobyjr.files.wordpress.com/2011/03/beer-95.jpg",
 		description: "This stout is brewed with a hint of coffee and vanilla then aged in oak bourbon barrels for over a year."
 		},
 		{
@@ -24,11 +24,20 @@ export default {
 		price: 7,
 		style: "Imperial Stout",
 		abv: 8.30,
-		imgUrl: "placeholder",
+		imgUrl: "https://craftbeerhaven.files.wordpress.com/2016/02/kingjulius.jpg?w=510",
 		description: "Exceptionally flavorful, juicy, and hop saturated beer while never tiring the palate."
 		},
 		{
 		id: 3,
+		item: "Pliny The Elder ",
+		price: 10,
+		style: "American Double / Imperial Stout",
+		abv: 8,
+		imgUrl: "http://p-fst2.pixstatic.com/506b0d13fb04d60a6b001298._w.1500_s.fit_.jpg",
+		description: "Considered the world's greatest Belgian Trappist beer. Made by drunk monks so you know it's good. "
+		},
+		{
+		id: 5,
 		item: "Pliny The Elder ",
 		price: 10,
 		style: "American Double / Imperial Stout",
@@ -42,25 +51,17 @@ export default {
 		price: 7,
 		style: "Imperial Stout",
 		abv: 8.30,
-		imgUrl: "placeholder",
+		imgUrl: "https://craftbeerhaven.files.wordpress.com/2016/02/kingjulius.jpg?w=510",
 		description: "Exceptionally flavorful, juicy, and hop saturated beer while never tiring the palate."
 		},
-		{
-		id: 5,
-		item: "Pliny The Elder ",
-		price: 10,
-		style: "American Double / Imperial Stout",
-		abv: 8,
-		imgUrl: "http://p-fst2.pixstatic.com/506b0d13fb04d60a6b001298._w.1500_s.fit_.jpg",
-		description: "Considered the world's greatest Belgian Trappist beer. Made by drunk monks so you know it's good. "
-		},
+		
 		{
 		id: 6,
 		item: "Pliny The Elder ",
 		price: 10,
 		style: "American Double / Imperial Stout",
 		abv: 8,
-		imgUrl: "placeholder",
+		imgUrl: "http://p-fst2.pixstatic.com/506b0d13fb04d60a6b001298._w.1500_s.fit_.jpg",
 		description: "Considered the world's greatest Belgian Trappist beer. Made by drunk monks so you know it's good. "
 		}
 	]
